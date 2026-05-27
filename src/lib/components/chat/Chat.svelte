0eee97c151473b93b7f93a47ae07fa1df44412ae
Algorithm5838 <108630393+Algorithm5838@users.noreply.github.com>
2026-05-27 10:10:58 +0300

    perf: remove leftover debug log in chatEventHandler

--- src/lib/components/chat/Chat.svelte
+++ src/lib/components/chat/Chat.svelte
@@ -465,8 +465,6 @@
 	};
 
 	const chatEventHandler = async (event, cb) => {
-		console.log(event);
-
 		if (event.chat_id === $chatId) {
 			await tick();
 			let message = history.messages[event.message_id];
